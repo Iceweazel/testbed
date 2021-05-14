6b8898270b24374d8092ac0d2825c86272044dc0
Johan Holm Åström <johaast@kth.se>
2021-05-14 13:19:03 +0200

    batch listen

--- consumers/src/main/java/com/mq/testbedconsumers/kafka/KafkaConsumer.java
+++ consumers/src/main/java/com/mq/testbedconsumers/kafka/KafkaConsumer.java
@@ -1,9 +1,12 @@
 package com.mq.testbedconsumers.kafka;
 
+import java.util.List;
+
 import com.mq.testbedconsumers.generics.AbstractConsumer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -12,8 +15,9 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumer extends AbstractConsumer {
 
     @KafkaListener(topics = topicName, containerFactory = "kafkaListenerContainerFactory")
-    public void consume(byte[] consumerRecord) {
-        handleContent(consumerRecord);
+    public void consume(@Payload List<byte[]> messages) {
+        for (byte[] consumerRecord : messages)
+            handleContent(consumerRecord);
     }
 
 }
