9ac7b1e738813078cd5716dd8ba3db222a84c652
Johan Holm Åström <johaast@kth.se>
2021-06-04 13:47:29 +0200

    mm

--- testbed/src/main/java/com/mq/testbedproducers/nats/NatsProducer.java
+++ testbed/src/main/java/com/mq/testbedproducers/nats/NatsProducer.java
@@ -1,6 +1,7 @@
 package com.mq.testbedproducers.nats;
 
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
 
 import com.mq.testbedproducers.generics.AbstractGenericProducer;
 
@@ -8,25 +9,41 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
-import io.nats.client.Connection;
-import io.nats.client.Nats;
-import io.nats.client.Options;
-import io.nats.client.Options.Builder;
+import io.nats.streaming.Options;
+import io.nats.streaming.Options.Builder;
+import io.nats.streaming.AckHandler;
+import io.nats.streaming.StreamingConnection;
+import io.nats.streaming.StreamingConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-// @Service
-// @ConditionalOnProperty(prefix = "testing", value = "mq", havingValue = "nats")
 public class NatsProducer extends AbstractGenericProducer {
 
     private final String uri;
-    private Connection natsConnection;
+    private StreamingConnection streamingConnection;
+    private AckHandler ackHandler;
+    private CountDownLatch ackDoneSignal;
 
     private static final String topicName = "ledger-1";
 
     public NatsProducer() {
         this.uri = "nats://localhost:4222";
-        this.natsConnection = initConnection();
+        startStreamingConnection();
+    }
+
+    private void startStreamingConnection() {
+        Options options = new Options.Builder().natsUrl(uri).clientId("producer").clusterId("nats-streaming").build();
+        StreamingConnectionFactory cf = new StreamingConnectionFactory(options);
+        ackDoneSignal = new CountDownLatch(1);
+        ackHandler = getAtLeastOnceAckHandler();
+
+        try {
+            streamingConnection = cf.createConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private Connection initConnection() {
@@ -49,7 +66,8 @@ public class NatsProducer extends AbstractGenericProducer {
     @Override
     public void publish(byte[] payload) {
         try {
-            natsConnection.publish(topicName, payload);
+            // natsConnection.publish(topicName, payload);
+            streamingConnection.publish(topicName, payload, ackHandler);
             log.debug("message published: {}", payload);
         } catch (Exception ioe) {
             log.error("Error publishing message: {} to {} ", payload.toString(), topicName, ioe);
@@ -59,10 +77,35 @@ public class NatsProducer extends AbstractGenericProducer {
     @Override
     public void close() {
         try {
-            natsConnection.close();
-        } catch (InterruptedException e) {
+            ackDoneSignal.await();
+            streamingConnection.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private AckHandler getAtLeastOnceAckHandler(){
+        return  new AckHandler() {
+            @Override
+            public void onAck(String nuid, String subject, byte[] data, Exception ex) {
+
+                if(ex != null){
+                    System.out.println("resend the message");
+                    try {
+                        streamingConnection.publish(topicName, data, ackHandler);
+                    } catch (Exception e){
+
+                    }
+                }
+
+                if(data.length == 1 && data[0] == '4') {
+                    ackDoneSignal.countDown();
+                }
+            }
+            @Override
+            public void onAck(String s, Exception e) {
+            }
+        };
     }
     
 }
