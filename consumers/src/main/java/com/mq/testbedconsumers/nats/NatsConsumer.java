7956379fef5aa73d701183b463c89e99c4a988e1
Johan Holm Åström <johaast@kth.se>
2021-06-04 14:34:23 +0200

    hhlo

--- consumers/src/main/java/com/mq/testbedconsumers/nats/NatsConsumer.java
+++ consumers/src/main/java/com/mq/testbedconsumers/nats/NatsConsumer.java
@@ -36,7 +36,7 @@ public class NatsConsumer extends AbstractConsumer {
     private void subscribe() {
         Options options = new Options.Builder().natsUrl(uri).clientId("consumer").clusterId("nats-streaming").build();
         StreamingConnectionFactory cf = new StreamingConnectionFactory(options);
-        SubscriptionOptions subOpts = new SubscriptionOptions.Builder().manualAcks().build();
+        SubscriptionOptions subOpts = new SubscriptionOptions.Builder().manualAcks().durableName("ledger-1").build();
         MessageHandler messageHandler = m -> {
             handleContent(m);
         };
