9b448f11f20898986a8d98008c78a56d5dca43e9
Johan Holm Åström <johaast@kth.se>
2021-06-09 10:51:14 +0200

    ative

--- separate/testbedproject/src/main/java/com/johan/consumers/ActiveConsumer.java
+++ separate/testbedproject/src/main/java/com/johan/consumers/ActiveConsumer.java
@@ -8,9 +8,11 @@ import javax.jms.MessageListener;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 
+import com.johan.generic.Consumer;
+
 import org.apache.activemq.ActiveMQConnectionFactory;
 
-public class ActiveConsumer {
+public class ActiveConsumer implements Consumer {
     
     private Connection connection;
     private Session session;
@@ -19,14 +21,37 @@ public class ActiveConsumer {
 
     public ActiveConsumer() {
         ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://localhost:61616");
-        ActiveListener listener = new ActiveListener();
 
         try {
             connection = factory.createConnection();
             connection.start();
             session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             topic = session.createTopic("ledger-1");
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void startListener() {
+        ActiveListener listener = new ActiveListener();
+        try {
             session.setMessageListener(listener);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void handleContent(byte[] payload) {
+        //should not need     
+    }
+
+    @Override
+    public void stopListener() {
+        try {
+            session.close();
+            connection.close();
         } catch (JMSException e) {
             e.printStackTrace();
         }
